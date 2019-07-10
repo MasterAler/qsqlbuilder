436ad47b637c4ba69096a2d0ac273bf29a8a753e
aler <akasyan@kraftway.ru>
2019-07-10 21:37:22 +0300

    + some doxxy added

--- sqlbuilder/Config.h
+++ sqlbuilder/Config.h
@@ -2,8 +2,24 @@
 
 #include <QString>
 
+/*!
+ * \brief The Config struct
+ * is a primitive wrapper for connection params.
+ * It could be somehow more sophisticated, with connection
+ * managing and all, but it's not the purpose of the library,
+ * so several static variables just store params, so that
+ * other classes (Query class to be exact) are not garbaged with them.
+ */
 struct Config
 {
+    /*!
+     * \brief setConnectionParams -- convenience method, sets all connection params at once
+     * \param driver              -- Qt driver choosing string, like "QPSQL"
+     * \param hostname            -- obviously, hostname
+     * \param dbname              -- obviously, database name
+     * \param username            -- obviously, db user name
+     * \param password            -- obviously, db user password
+     */
     static void setConnectionParams(const QString& driver, const QString& hostname, const QString& dbname
                                     , const QString& username, const QString& password)
     {
